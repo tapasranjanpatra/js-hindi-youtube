c8a51b037c14123c53c0fccb1bd9afa28c7114c6
TRP <124392653+tapasranjanpatra@users.noreply.github.com>
2023-09-26 10:44:05 +0000

    completed variables and constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553//can not change the value
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"//not use var
+accountCity="jaipur"//this is not good practice
+let accountState;
+// accountId=2 not allowed
+
+accountEmail="trp@gmail.com"
+accountPassword="21212121"
+accountCity="bengaluru"
+console.log(accountId);
+console.table([accountId,accountState,accountEmail,accountPassword,accountCity])
+
+/*
+this is new comment 
+perfer not use var
+console.table([])
+*/
