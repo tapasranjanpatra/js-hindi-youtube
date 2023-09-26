2917204758d9fcb4dbadd6e9877b98ce81769757
TRP <124392653+tapasranjanpatra@users.noreply.github.com>
2023-09-26 17:13:02 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="hitesh"
+console.log(typeof score)
+console.log(typeof(score))
+let value=Number(score)
+console.log(typeof(value))
+console.log(value)
+//"33"=>33 it will easy to conversion a number
+//"33asb"=>NaN
+//true=>1;false=>0  
+//"hitesh"=>string it can not change in to number
+/*************************************** */
+let isLoggedIn="hitesh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn)
+//booleans conversion
+//1=>true 0=>false
+//""=>false
+//"hitesh"=>true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
