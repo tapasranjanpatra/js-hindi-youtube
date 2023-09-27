a2541c480a627e8ade34506dd535d26a2f865ad6
TRP <124392653+tapasranjanpatra@users.noreply.github.com>
2023-09-27 16:19:27 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log("2">1)//=>true
+// console.log("02">1)//=>true
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// ===
+console.log("2"==2);
+console.log("2"===2);
+// to avoid the above syntax
