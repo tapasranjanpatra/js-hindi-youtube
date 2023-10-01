364227502ad525e5050752ea006b44a530d67118
TRP <124392653+tapasranjanpatra@users.noreply.github.com>
2023-10-01 06:22:24 +0000

    02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,20 +1,20 @@
 const marvel_heros=["thor","ironman","spiderman"]
 const dc_heros=["superman","flash","batman"]
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)=>array inside another array
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
-// const allheros=marvel_heros.concat(dc_heros)
+// const allheros=marvel_heros.concat(dc_heros)=>combined array
 // console.log(allheros);
-const all_new_heros=[...marvel_heros,...dc_heros]
+const all_new_heros=[...marvel_heros,...dc_heros]//=>spread(...)
 console.log(all_new_heros);
-
+//********** */
 const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array=another_array.flat(Infinity)
 console.log(real_another_array);
   //****** */
 console.log(Array.isArray())
 console.log(Array.from("HITESH"))
-console.log(Array.from({name:"HITESH"}))//interesting
+console.log(Array.from({name:"HITESH"}))//interesting[]
 // *****
 let score1=100
 let score2=200
