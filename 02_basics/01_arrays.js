ebcbcfa609cb4b4582ed7b42802e29d476d8abe6
TRP <124392653+tapasranjanpatra@users.noreply.github.com>
2023-10-01 06:16:40 +0000

    02_array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -18,11 +18,11 @@ const arr2=new Array(5,6,4,5,8,7,4,)
 // console.log(newarray);
 //slice,splice(interview question)
 const myarr=[0,1,2,3,4,5,6]
-console.log("A",myarr);
+// console.log("A",myarr);
 const myn1=myarr.slice(1,3)
-console.log(myn1);
-console.log("B",myarr);//=>array will no change 0,1,2,3,4,5,6
+// console.log(myn1);
+// console.log("B",myarr);//=>array will no change 0,1,2,3,4,5,6
 // ******
 const myn2=myarr.splice(1,3)
-console.log("C",myarr);//=>array change 0,4,5,6
-console.log(myn2);
+// console.log("C",myarr);//=>array change 0,4,5,6
+// console.log(myn2);
