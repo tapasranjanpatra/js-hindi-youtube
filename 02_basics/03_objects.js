85087073c3b41270161ec5dc7004b37ac3713dca
TRP <124392653+tapasranjanpatra@users.noreply.github.com>
2023-10-01 11:19:59 +0000

    object.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,12 +1,28 @@
-// singleton
+
 //object laterals
+ const mysym=Symbol("mykey1")// //how to use symbol inside object and access it.
 const jsuser={
     name:"hitesh",
+    "full name":"hitesh bimal",
+     [mysym]:"mykey1",
     age:18,
     location:"jaipur",
     email:"hitesh@google.com",
-    isLoggedIn:false,
     lastLoginDays:["monday","saturday"]
 }
-console.log(jsuser.email)
-console.log(jsuser["email"])
+jsuser.email="hitesh@chatgpt.com"
+// Object.freeze(jsuser)//=>no change after 
+jsuser.email="hitesh@microsoft.com"
+// console.log(jsuser);
+// console.log(jsuser.email);
+// console.log(jsuser["email"]);
+// console.log(jsuser["full name"]);
+// console.log(typeof jsuser[mysym]);
+jsuser.greeting=function(){
+    console.log("hello js user");
+}
+jsuser.greetingtwo=function(){
+    console.log(`hello js user,${this.name}`);
+}
+console.log(jsuser.greeting());
+console.log(jsuser.greetingtwo());
