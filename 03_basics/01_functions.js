b71c65570964c1d76bfa7706018b306ee732e4c2
TRP <124392653+tapasranjanpatra@users.noreply.github.com>
2023-10-02 07:31:39 +0000

    01_functions.js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,35 @@
+
+function saymyname(){
+    console.log("T");
+    console.log("A");
+    console.log("P");
+    console.log("A");
+    console.log("S");
+}
+// saymyname()
+//********* */
+// function addtwonumber(number1,number2){//parametre
+//       console.log(number1+number2);
+// }
+// const result=addtwonumber(3,5)//argument
+// console.log("result:",result);
+function addtwonumber(number1,number2){
+//    let result=number1+number2
+//    return result
+//    console.log("hitesh")//this is not execute bcz ret.done
+
+return number1+number2
+}
+const result=addtwonumber(3,5)
+//  console.log("result:",result);
+    // *************
+    function loginusermessage(username){
+        return `${username} just logged in`
+    }
+console.log(loginusermessage("hitesh")
+);
+function username(){
+    console.log("hitesh bimal")
+}
+const username1=username()
+console.log("final result:",username1);
